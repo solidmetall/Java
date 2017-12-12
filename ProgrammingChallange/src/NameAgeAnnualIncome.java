8ca68cbc3b3096dafd0ef68a355c9295aa861723
Evgeniy Petin <34118433+solidmetall@users.noreply.github.com>
2017-12-12 12:54:01 -0800

    Add files via upload

---
+++ ProgrammingChallange/src/NameAgeAnnualIncome.java
@@ -0,0 +1,29 @@
+
+public class NameAgeAnnualIncome {
+
+	public static void main(String[] args) {
+		/*String name;
+		int age;
+		double annualPay;
+		
+		name = "Evgeniy M. Petin";
+		age = 24;
+		annualPay = 160000;
+		
+		System.out.println("My name is " + name +", my age is "+ age + " and\nI hope to earn $"+annualPay+" per year.");
+		//System.out.println("I hope to earn $"+annualPay+" per year.");*/
+		
+		String firstName, middleName, lastName;
+		firstName = "Evgeniy";
+		middleName = "Mikhailovich";
+		lastName = "Petin";
+		
+		char firstInitial, middleInitial, lastInitial;
+		firstInitial = 'E';
+		middleInitial = 'M';
+		lastInitial = 'P';
+		
+		System.out.println("My name is: "+firstName+" "+middleName+" "+lastName+ '\n' + "My initials are: "+firstInitial+middleInitial+lastInitial);
+	}
+
+}
